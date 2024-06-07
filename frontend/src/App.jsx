61a5263daf5b7fd52a2b04b3d5d88d752c5f8fdb
ekrispy <estevan.crispin@gmail.com>
2024-06-07 11:13:12 -0600

    added privateroute to app.jsx, added authprovider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './Context/Context';
 import Home from './Components/Home/Home';
 import Container from './Components/Container';
 import Login from './Components/Login/Login';
 import SignUp from './Components/SignUp/SignUp';
+import PrivateRoute from './Components/PrivateRoutes/PrivateRoutes';
 
 function App() {
   return (
     <main className="flex items-center justify-center h-screen">
+      <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/home" element={<Container />} />
+        <PrivateRoute path="/home" element={<Container />} />
       </Routes>
+      </AuthProvider>
     </main>
   );
 }
